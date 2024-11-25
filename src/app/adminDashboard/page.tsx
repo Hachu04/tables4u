51efb28713dc026c89ddfb3ac9a716a9ff378611
TheRiceWaterBoi <tsuweichang8888@gmail.com>
2024-11-24 19:18:51 -0500

    Creates admin dashboard page

---
+++ src/app/adminDashboard/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React, { useState } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+
+
+export default function adminDashboard(){
+
+    return(
+        <button type="submit" className="w-full bg-blue-500 text-white py-3 rounded hover:bg-blue-600 transition">
+        List Restaurants
+      </button>
+    )
+
+}
