c36c22920bac1fca5ea87c2de7d53c850e5f681f
DylanSerreyn <dsserreyn@wpi.edu>
2024-11-26 17:10:38 -0500

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,53 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export',
+  // basePath: '/tables4u',
+  // assetPrefix: '/tables4u',
+  // trailingSlash: true,
 };
 
+// module.exports = {
+//   output: 'export',
+//   basePath: '/tables4u',
+//   assetPrefix: '/tables4u',
+//   trailingSlash: true,
+//   redirects() {
+//     return [
+//       {
+//         source: '/tables4u/restaurantLogin',
+//         destination: '/tables4u/restaurantLogin/index.html',
+//         permanent: true, // Set to true for permanent redirect
+//       },
+//       {
+//         source: '/tables4u/restaurantDashboard',
+//         destination: '/tables4u/restaurantDashboard/index.html',
+//         permanent: true, // Set to true for permanent redirect
+//       },
+//       {
+//         source: '/tables4u/createRestaurant',
+//         destination: '/tables4u/createRestaurant/index.html',
+//         permanent: true, // Set to true for permanent redirect
+//       },
+//       {
+//         source: '/tables4u/adminDashboard',
+//         destination: '/tables4u/adminDashboard/index.html',
+//         permanent: true, // Set to true for permanent redirect
+//       },
+//       {
+//         source: '/tables4u/adminLogin',
+//         destination: '/tables4u/adminLogin/index.html',
+//         permanent: true, // Set to true for permanent redirect
+//       },
+//       {
+//         source: '/tables4u/404',
+//         destination: '/tables4u/404/index.html',
+//         permanent: true, // Set to true for permanent redirect
+//       },
+//     ];
+//   },
+// };
+
+
 export default nextConfig;
+
